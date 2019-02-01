b80c2401dfcf066f9f50d0b6193a6fdc96bf3da4
tmskrtsz <tamas@tuta.io>
2019-02-01 20:02:51 +0200

    Move Switch to header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
+import { rgba } from 'polished'
 
 import { Consumer } from '../../store'
-import theme from '../../theme'
-import { Container, Row, Column, Inner } from './Grid'
+import Switch from './Switch'
+import theme from '../../theme/light'
+import { Container, Row, Column } from './Grid'
 import logo from '../../images/logo.svg'
 
+import _throttle from 'lodash.throttle'
+
 const Header = styled.header`
-	background-color: ${ props => props.theme.color.bg };
+	background-color: ${ props => (props.active ? rgba(props.theme.color.bg, 0.97) : 'transparent') };
+	border-top: 2px solid ${ props => props.theme.color.primary };
+	padding: ${ props => (props.active ? '0.4em' : '1.2em') } 0;
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	z-index: 1000;
+	box-shadow: 0 4px 30px ${ props => (props.active ? rgba(props.theme.color.dark, 0.12) : 'transparent') };
+	transition: all 0.2s ease-in-out;
+
+	${ Row } {
+		margin-left: 0;
+		margin-right: 0;
+	}
 `
 
 const Home = styled(Link)`
@@ -22,14 +40,16 @@ const Home = styled(Link)`
 `
 
 const Navbar = styled.nav`
-	background-color: ${ props => props.theme.color.bg };
+	display: flex;
+	align-items: center;
+	justify-content: center;
 
 	a {
-		margin-left: 3em;
-		color: #000;
+		color: ${ props => props.theme.color.text };
 		font-size: 1.4rem;
 		text-transform: uppercase;
 		letter-spacing: 1.5px;
+		padding: 0 1.8em;
 	}
 `
 
@@ -37,40 +57,70 @@ const active = {
 	color: theme.color.primary
 }
 
-export default () => (
-	<Header>
-		<Container>
-			<Row align="center">
-				<Column>
-					<Inner>
-						<Home to="/">Home</Home>
-					</Inner>
-				</Column>
-				<Column style={{ marginLeft: 'auto' }}>
-					<Inner>
-						<Navbar>
-							<Link
-								activeStyle={active}
-								to="/#work"
-							>
-								Work
-							</Link>
-							<Link
-								activeStyle={active}
-								to="/about"
-							>
-								<Consumer>{({ douche }) => (douche ? 'About Him' : 'About')}</Consumer>
-							</Link>
-							<Link
-								activeStyle={active}
-								to="/acontact"
-							>
-								Contact
-							</Link>
-						</Navbar>
-					</Inner>
-				</Column>
-			</Row>
-		</Container>
-	</Header>
-)
+export default class extends Component {
+	state = {
+		scrolling: false
+	}
+
+	isScrolling = () => {
+		if (window.pageYOffset > 0) {
+			this.setState({ scrolling: true })
+		} else {
+			this.setState({ scrolling: false })
+		}
+	}
+
+	componentDidMount () {
+		window.addEventListener('scroll', _throttle(this.isScrolling, 250))
+	}
+
+	componentWillUnmount () {
+		window.removeEventListener('scroll', this.isScrolling)
+	}
+
+	render () {
+		const { scrolling } = this.state
+		return (
+			<Header active={scrolling}>
+				<Container>
+					<Row
+						align="center"
+						grid={1 / 3}
+					>
+						<Column>
+							<Home to="/">Home</Home>
+						</Column>
+						<Column>
+							<Navbar>
+								<Link
+									activeStyle={active}
+									to="/#work"
+								>
+									Work
+								</Link>
+								<Link
+									activeStyle={active}
+									to="/about"
+								>
+									<Consumer>{({ douche }) => (douche ? 'About Him' : 'About')}</Consumer>
+								</Link>
+								<Link
+									activeStyle={active}
+									to="/acontact"
+								>
+									Contact
+								</Link>
+							</Navbar>
+						</Column>
+						<Column style={{ marginLeft: 'auto' }}>
+							<Consumer>{({ actions, douche }) => <Switch
+								onClick={actions.switchTone}
+								status={douche}
+							/>}</Consumer>
+						</Column>
+					</Row>
+				</Container>
+			</Header>
+		)
+	}
+}
