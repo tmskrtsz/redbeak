7c61a2b4d37ed6ad485d62baa0c5b9172245a313
tmskrtsz <tamas.kertesz@anders.fi>
2019-02-01 20:03:59 +0200

    Add theme support

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import lightTheme from '../theme/light'
+import darkTheme from '../theme/dark'
 
 const { Consumer, Provider } = React.createContext()
 
@@ -8,18 +10,34 @@ class Store extends Component {
 
 		this.state = {
 			douche: false,
+			theme: null,
 			actions: {
 				switchTone: this.switchTone
 			}
 		}
 	}
 
-	switchTone = () => {
-		this.setState({ douche: !this.state.douche })
+	switchTone = async () => {
+		await this.setState({
+			douche: !this.state.douche
+		})
+
+		if (!this.state.douche) {
+			await this.setState({ theme: lightTheme })
+		} else {
+			await this.setState({ theme: darkTheme })
+		}
+	}
+
+	componentDidMount () {
+		if (!this.state.theme) {
+			this.setState({ theme: lightTheme })
+		}
 	}
 
 	render () {
 		const { children } = this.props
+		if (!this.state.theme) return null
 		return <Provider value={this.state}>{children}</Provider>
 	}
 }
---
+++ src/theme/dark.js
@@ -0,0 +1,11 @@
+import constant from './constant'
+
+export default {
+	color: {
+		bg: '#2e2c35',
+		primary: '#F73839',
+		dark: '#232323',
+		text: '#9d9ba5'
+	},
+	...constant
+}
