98c3dbdb3016aed333ad98889384cbf815c9170e
tmskrtsz <tamas.kertesz@anders.fi>
2019-02-03 23:06:38 +0200

    Make themes more sane

--- src/theme/constant.js
+++ src/theme/constant.js
@@ -1,14 +1,14 @@
 export default {
-	text: {
-		fontFamily: 'acumin-pro, sans-serif',
-		baseFontSize: '1.8rem'
-	},
-	breakpoints: {
-		sm: '532px',
-		md: '768px',
-		lg: '1180px'
-	},
-	space: {
-		gutter: 1.2
-	}
+  text: {
+    fontFamily: 'acumin-pro, sans-serif',
+    baseFontSize: '1.8rem'
+  },
+  breakpoints: {
+    sm: '532px',
+    md: '768px',
+    lg: '1180px'
+  },
+  space: {
+    gutter: 1.2
+  }
 }
