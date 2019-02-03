fc5c737370805f801509419509b649679f52f2af
tmskrtsz <tamas@tuta.io>
2019-02-03 23:06:38 +0200

    Make themes more sane

--- src/theme/dark.js
+++ src/theme/dark.js
@@ -1,11 +1,17 @@
 import constant from './constant'
 
 export default {
-	color: {
-		bg: '#2e2c35',
-		primary: '#F73839',
-		dark: '#232323',
-		text: '#9d9ba5'
-	},
-	...constant
+  color: {
+    bg: '#2e2c35',
+    primary: '#F73839',
+    dark: '#232323',
+    text: '#bebcc5',
+    bannerBg: '#29272f'
+  },
+  button: {
+    bg: '#fff',
+    border: '#757180',
+    color: '#bebcc5'
+  },
+  ...constant
 }
--- src/theme/light.js
+++ src/theme/light.js
@@ -1,11 +1,17 @@
 import constant from './constant'
 
 export default {
-	color: {
-		bg: '#fff',
-		primary: '#F73839',
-		dark: '#232323',
-		text: '#000'
-	},
-	...constant
+  color: {
+    bg: '#fff',
+    primary: '#F73839',
+    dark: '#232323',
+    text: '#000',
+    bannerBg: '#f9f9f9'
+  },
+  button: {
+    bg: '#fff',
+    border: '#e2e2e2',
+    color: '#000'
+  },
+  ...constant
 }
