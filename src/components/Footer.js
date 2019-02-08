882d16b3996bca7da3d6fe7ec4f1eb1699e483a3
tmskrtsz <tamas@tuta.io>
2019-02-08 21:47:36 +0200

    Complete footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Consumer } from '../store'
 
 import { Wrapper, Container, Row, Column } from './ui/Grid'
 
-const FooterStyle = styled.footer``
+const FooterStyle = styled(Wrapper)``
 
 const Footer = () => (
-  <Wrapper>
+  <FooterStyle as="footer">
     <Container>
       <Row>
-        <Column>Hey</Column>
+        <Consumer>
+          {({ douche }) =>
+            douche ? (
+              <Column align="center">Artisanally handcrafted with lots and lots of ❤️ and ☕</Column>
+            ) : (
+              <Column align="center">Developed in sunny Finland</Column>
+            )
+          }
+        </Consumer>
+      </Row>
+      <Row>
+        <Column align="center">
+          <p>
+            Made with{' '}
+            <a
+              href="https://github.com/tmskrtsz/redbeak"
+              target="_blank"
+              rel="nofollow"
+            >
+              Gatsby
+            </a>
+          </p>
+        </Column>
       </Row>
     </Container>
-  </Wrapper>
+  </FooterStyle>
 )
 
 export default Footer
