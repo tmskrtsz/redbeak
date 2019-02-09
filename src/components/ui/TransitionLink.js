d17131bd12429eefe5b6578425a9c958e836dd06
tmskrtsz <tamas@tuta.io>
2019-02-09 15:27:23 +0200

    Slight change to buttons

--- src/components/ui/TransitionLink.js
+++ src/components/ui/TransitionLink.js
@@ -6,33 +6,33 @@ import { rgba, timingFunctions } from 'polished'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
 import theme from '../../theme/light'
 
+const HoverStyle = css`
+  position: relative;
+  cursor: pointer;
+
+  &::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 35%;
+    background-color: ${ props => rgba(props.theme.color.primary, 0.2) };
+    z-index: -1;
+    transform: scaleX(0);
+    transform-origin: left;
+  }
+
+  :hover {
+    &::before {
+      transform: scaleX(1);
+      transition: transform 0.25s ${ timingFunctions('easeOutQuad') };
+    }
+  }
+`
+
 const LinkStyle = styled(AniLink)`
-  ${ props =>
-    props.anchor &&
-    css`
-      position: relative;
-      cursor: pointer;
-
-      &::before {
-        content: '';
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        height: 35%;
-        background-color: ${ props => rgba(props.theme.color.primary, 0.2) };
-        z-index: -1;
-        transform: scaleX(0);
-        transform-origin: left;
-      }
-
-      :hover {
-        &::before {
-          transform: scaleX(1);
-          transition: transform 0.25s ${ timingFunctions('easeOutQuad') };
-        }
-      }
-    ` }
+  ${ props => props.anchor && HoverStyle }
 `
 
 const Link = ({ children, color, ...other }) => (
@@ -54,4 +54,4 @@ Link.propTypes = {
   anchor: PropTypes.bool
 }
 
-export { Link }
+export { Link, HoverStyle }
