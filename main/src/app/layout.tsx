ab9e84f30119e07dc46aebedd292edf9574aab21
tmskrtsz <tamas.kertesz@happy-or-not.com>
2023-07-26 12:56:40 +0300

    feat: update styles

--- main/src/app/layout.tsx
+++ main/src/app/layout.tsx
@@ -52,7 +52,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={cx(local.variable, inter.variable, 'gray-theme')}>
+    <html lang="en" className={cx(local.variable, inter.variable, 'redbeak')}>
       <body>
         <main>
           <Nav />
