873f1eb47e183b03a12d8a5cf13ef44915ff5d87
tmskrtsz <tamas.kertesz@happy-or-not.com>
2023-12-29 15:39:20 +0200

    fixup footer

--- packages/main/src/features/cta.tsx
+++ packages/main/src/features/cta.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import styles from './cta.module.css'
 import { FadeUp } from '../components/fadeup';
-import { Button } from '../components/button';
+import Image from 'next/image';
+import logoImage from '../../public/logo.png';
 
 export function CTA() {
   return (
     <FadeUp offset={'end'}>
       <div className={styles.container}>
-        dsa
-        <Button>Email Me</Button>
+      <Image src={logoImage} alt="redbeak logo" width={48} />
+        <a className={styles.mailto} href="mailto:tamask@hey.com?subject=Let's connect!">Email Me</a>
       </div>
     </FadeUp>
   );
